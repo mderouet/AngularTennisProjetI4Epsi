422103ff28980a6a55cf834b7aa5f10be1f596f9
Akididou <jp.poizat@hotmail.fr>
2017-05-25 14:53:34 +0200

    HP Dynamique

---
+++ src/app/pages/rencontres/rencontres.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {RequestService} from "../../services/request.service";
+import {Config} from "../../config/config";
+import {Joueur} from "../../services/joueur.service";
+
+@Component({
+    selector: 'joueurs',
+    templateUrl: '/app/pages/joueur/joueur.html',
+    providers: [RequestService, Config, Joueur],
+    styleUrls:  ['./joueur.component.css']
+})
+export class JoueurComponent implements OnInit {
+    joueurs: [JSON];
+    joueur: string;
+    valueConfig: String;
+
+    constructor(public requestService: RequestService) {
+
+    }
+
+    ngOnInit() {
+        this.chargerJoueurs();
+    }
+
+  //Charge la liste des joueurs et les données qui sont liés
+  chargerJoueurs() {
+    this.requestService.listJoueur().subscribe((joueurs) => {
+      this.joueurs = joueurs;
+      for(var j in joueurs) {
+        console.log(joueurs[j]);
+      }
+    });
+  }
+}

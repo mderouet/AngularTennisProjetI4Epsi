bcc985d5a58d6a57c0a681d7c281c7f8f19e4830
Akididou <jp.poizat@hotmail.fr>
2017-06-06 01:44:08 +0200

    detail points

--- src/app/pages/resultat_details/resultat_details.component.ts
+++ src/app/pages/resultat_details/resultat_details.component.ts
@@ -11,30 +11,71 @@ import {RequestService} from '../../services/request.service';
 })
 
 export class ResultatDetails implements OnInit {
-
-    scoreRencontre:[JSON];
+    rencontres: [JSON];
+    scoreRencontre: [JSON];
     private sub: any;
     idResultat: number;
+    points: Array<any> = [];
+    nbrPoints = 0;
 
     constructor(public requestService: RequestService, private route: ActivatedRoute, private router: Router) {
 
     }
 
     ngOnInit() {
-      this.sub = this.route.params.subscribe(params => {
-        this.idResultat = +params['id']; // (+) converts string 'id' to a number
-        this.chargerScore(this.idResultat);
-      });
+        this.sub = this.route.params.subscribe(params => {
+            this.idResultat = +params['id'] - 1; // (+) converts string 'id' to a number
+            console.log("id =" + this.idResultat)
+            this.chargerRencontres();
+
+
+        });
     }
 
-  chargerScore(id:number) {
-    this.requestService.showScore(id).subscribe((scoreRencontre) => {
-      // Local value
-      this.scoreRencontre = scoreRencontre;
+    chargerScore(id: number) {
+        this.requestService.showScore(id).subscribe((scoreRencontre) => {
+            // Local value
+            this.scoreRencontre = scoreRencontre;
+            this.chargerHeure();
+        });
+    };
 
-      console.log(scoreRencontre);
-    });
-  }
+    chargerRencontres() {
+        this.requestService.listRencontres().subscribe((rencontres) => {
+            // Local value
+            this.rencontres = rencontres;
+            this.chargerScore(this.idResultat + 1);
+
+        });
+
+    }
+
+    chargerHeure() {
+        // console.log(this.scoreRencontre);
+        // console.log("debut");
+        // for (var i = 0; i < this.scoreRencontre.length; i++) {
+        //     console.log(this.scoreRencontre[i].rencontre.sets[0].jeux[3].jeu.id);
+        //     for (var j = 0; j < this.rencontres[i].rencontre.sets.length; j++) {
+        //         console.log(this.scoreRencontre[i].rencontre.sets[j].set.id)
+        //     }
+        // }
+        // console.log("fin");
+        let self = this;
+
+        this.scoreRencontre.forEach(function (currentRencontre) {
+            console.log(currentRencontre);
+            currentRencontre.rencontre.sets.forEach(function (currentSets) {
+                console.log(currentSets);
+                currentSets.jeux.forEach(function (currentJeux) {
+                    console.log(currentJeux);
+                    currentJeux.jeu.points.forEach(function (currentPoints) {
+                        self.points.push(currentPoints);
+                        console.log(self.points)
+                    });
+                });
+            });
+        });
+    }
 }
 
 
